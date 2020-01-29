3b2406bea27d437ef85dc05121a1841f6e85097a
Ritveak Dugar <ritveak@myidos.com>
2020-01-29 16:20:23 +0530

    Added rhymer dictionary complete

--- Wordzone.py
+++ Wordzone.py
@@ -425,7 +425,65 @@ class FRhyOpt(Screen):
             return "No rhyming words were found, try entering a different word."
 
 class FRhyMeanInput(Screen):
-    pass
+    def rhymerdic(self,iword,mword) :
+        # a proper module separately made for finding rhyming words; also based on cmudict.
+        fb = pronouncing.rhymes(iword)
+        #Simple synonym set
+        rr=[]
+        
+        if(mword=="*"):
+            print(fb)
+            sys.exit()
+        syns=wordnet.synsets(mword)
+        for syn in syns:
+            rr+=syn.lemma_names()
+
+        #Since simple synonym set is not enough, lets add more related words 
+        #finding all related words among which rhyming words is to be found
+
+        hr=[]
+        syns=wordnet.synsets(mword)
+        for syn in syns:
+            sn=syn.hypernyms()#broader category:colour is a hypernym of red.
+            an=syn.hyponyms() #narrower category - red : color
+            dn=syn.member_holonyms()#Body is a holonym of arm, leg and heart
+            for s in sn:
+                hr+=s.lemma_names()
+            for a in an:
+                hr+=a.lemma_names()
+            for d in dn:
+                hr+=d.lemma_names()
+                
+        #now even "loaf" gets included when "food" is given as input
+
+        #making the list richer by adding synonyms of the words which are in hr.
+        fn=[]
+        for h in hr:
+            ss=wordnet.synsets(h)
+            for s in ss:
+                fn+=s.lemma_names()
+
+        fn = list(dict.fromkeys(fn)) # removing duplicates
+
+            
+        #now selecting only the words that are common in both
+
+        fo = list(set(fb)&set(fn))
+        for chk in fb:
+            for chk1 in fn:
+                my_regex = r".*" + re.escape(chk) + r"$"
+                found =(re.search(my_regex ,chk1, re.M|re.I))
+                if found:
+                    fo.append(found.group())
+        fo=list(set(fo))
+
+        new = "" 
+        for x in fo: 
+            new += x+"\n"
+            #add more details like pos and all
+        print(new) 
+        return new
+
 class FRhyMeanRes(Screen):
     pass
 class FRhyDirectRes(Screen):
