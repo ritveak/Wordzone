c30386b28cc4184a172383507c5591971cf554e8
krishnakrmahto97 <krish.thorcode@gmail.com>
2020-03-29 22:15:23 +0530

    added predict function in model_util.py (earlier this file was named as train.py)

--- model_util.py
+++ model_util.py
@@ -82,3 +82,32 @@ def train_and_test(data_path):
   prediction = prediction.reshape((-1))
 
   return prediction
+
+  def predict(data_path, model, num_prediction=1):
+
+    with open(data_path, 'rb') as f:
+      data_list = pickle.load(f)
+
+    df = pd.DataFrame(data_list, columns=['frequency'])
+
+    frequency = df['Close'].values
+
+    prediction_list = frequency[-look_back:]
+    
+    for _ in range(num_prediction):
+        x = prediction_list[-look_back:]
+        x = x.reshape((1, look_back, 1))
+        out = model.predict(x)[0][0]
+        prediction_list = np.append(prediction_list, out)
+    prediction_list = prediction_list[look_back-1:]
+        
+    return prediction_list
+    
+def predict_dates(num_prediction):
+    last_date = df['Date'].values[-1]
+    prediction_dates = pd.date_range(last_date, periods=num_prediction+1).tolist()
+    return prediction_dates
+
+num_prediction = 30
+forecast = predict(num_prediction, model)
+forecast_dates = predict_dates(num_prediction)
